a44bdfe6e8139171a79b6a5bae9e7db8e9215648
nitik89 <nitiknarang77@gmail.com>
2024-03-14 01:14:18 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ dotenv.config();
 const { MONGO_URL } = process.env;
 const PORT = 8000;
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, "::1", () => {
   console.log(`${PORT}`);
 });
 
@@ -15,12 +15,26 @@ app.use(cors());
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "http://192.168.1.8:3000",
+      "http://192.168.1.3:3000",
+    ],
   },
 });
 let teams = [];
+let players = [];
 io.on("connection", (socket) => {
   console.log("connected to socket.io");
+
+  const decreasePurse = (teamName, amount) => {
+    const team = teams.find((team) => team.name === teamName);
+    if (team) {
+      team.purse -= amount;
+      return true; // Indicates team found and purse decreased
+    }
+    return false; // Indicates team not found
+  };
 
   socket.on("joinRoom", ({ name, logo, purse, roomId }) => {
     console.log(name, roomId);
@@ -33,19 +47,26 @@ io.on("connection", (socket) => {
     });
   });
 
-  socket.on("bid-player", ({ price, team, bids }) => {
-    console.log(price, team, bids);
-    io.emit("team-bid", { price, team, bids });
+  socket.on("bid-player", ({ price, team, bids, logo }) => {
+    io.emit("team-bid", { price, team, bids, logo });
   });
   socket.on("unsold-player", ({ currPlayer, currIdx }) => {
     io.emit("player-unsold", { currPlayer, currIdx });
   });
   socket.on("withraw-bid", ({ team, bids, currPlayer, currIdx }) => {
-    console.log("withdrawn by iteseld", currPlayer);
-    io.emit("withdrawn-bid", { bids, team, currPlayer, currIdx });
+    const keys = Object.keys(bids);
+    const findPlayer = players.find(
+      (player) =>
+        player.name == currPlayer.name && player.logo == currPlayer.logo
+    );
+    if (keys.length == 1 && !findPlayer) {
+      decreasePurse(keys[0], bids[keys[0]]);
+      players.push(currPlayer);
+    }
+
+    io.emit("withdrawn-bid", { bids, team, currPlayer, currIdx, teams });
   });
   socket.on("disconnect", () => {
-    console.log("User disconnected", socket.teamName);
     if (socket.teamName) {
       teams = teams.filter((team) => team.name !== socket.teamName);
       io.emit("team-left", {
